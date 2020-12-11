b4905c63ca2e818d3dbc8e302e88769364be240b
hilderneyduesoft <hilderney@duesoft.com.br>
2020-12-11 18:02:58 -0300

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SpinnerComponent } from './components/shared/spinner/spinner.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { SpinnerComponent } from './components/shared/spinner/spinner.component'
     MaterialModule,
   ],
   exports: [
-    SpinnerComponent,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
